9ffe57c1692da3d9e6d2f6b4ab134858048e215b
JoseMondragon <j.mondragon@hotmail.com>
2022-06-08 18:29:33 -0400

    parsing JSON file, AB#374

--- src/com/games/gobigorgohome/Gym.java
+++ src/com/games/gobigorgohome/Gym.java
@@ -1,5 +1,58 @@
 package com.games.gobigorgohome;
 
-class Gym {
+import java.io.FileReader;
+import java.util.Iterator;
+import java.util.Map;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*;
+
+
+public class Gym {
+
+    public void setRooms(JSONObject rooms) {
+        this.rooms = rooms;
+    }
+
+    private JSONObject rooms;
+
+
+
+    public static void main(String[] args) throws Exception {
+        // parsing file "JSONExample.json"
+        JSONParser parser = new JSONParser();
+        try {
+        Object obj = parser.parse(new FileReader("C:\\AmazonSDE\\Practical\\prac1\\GoBigGoHome\\gymRooms.json"));
+            // typecasting obj to JSONObject
+            JSONObject jo = (JSONObject) obj;
+            // getting every room
+            JSONObject rooms = (JSONObject) jo.get("rooms");
+            Object frontDesk = rooms.get("front desk");
+            System.out.println(frontDesk);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private void getCurrentRoom(String room){
+        rooms.get("room");
+
+
+
+
+
+
+    }
 }
+
+
+
+
+
+
+
+
