20a08f446befaf872d9df69cd3789790bc0b7c8f
JoseMondragon <j.mondragon@hotmail.com>
2022-06-09 18:27:30 -0400

    ASCII art and instructions AB#252, AB#377

---
+++ src/com/games/gobigorgohome/SplashPage.java
@@ -0,0 +1,31 @@
+package com.games.gobigorgohome;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+
+public class SplashPage {
+
+    public void createAscii(String fileName) {
+        try {
+            Files.lines(Path.of(fileName))
+                    .forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void instructions(){
+        createAscii("resources/banner.txt");
+        System.out.println("\033[33;1;2mWELCOME TO 'GO BIG OR GO HOME!'\033[0m\nA game by \033[33;1;2mGAINZZZ Productions\033[0m where you maybe learn " +
+                "to use your gym time wisely and get BIG!");
+        System.out.println("\033[31;4;1mINSTRUCTIONS:\033[0m");
+        createAscii("resources/instructions.txt");
+        System.out.println("\033[31;4;1m!!!IMPORTANT!!!:\033[0m");
+        createAscii("resources/instructions2.txt");
+
+    }
+}
+
+
+}
