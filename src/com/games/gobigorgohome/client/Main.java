671dd12dc80f43d7440d3c4884360e41345f5d1a
samekh_resh <C0d1ng4Lyf3>
2022-06-13 15:22:12 -0400

    pushing so marcos can pull

--- src/com/games/gobigorgohome/client/Main.java
+++ src/com/games/gobigorgohome/client/Main.java
@@ -7,7 +7,7 @@ import org.json.simple.parser.ParseException;
 import java.io.IOException;
 import java.util.Scanner;
 
-class Main {
+public class Main {
     public static void main(String[] args) throws IOException, ParseException {
         Game app = new Game(new Prompter(new Scanner(System.in)));
         app.playGame();
