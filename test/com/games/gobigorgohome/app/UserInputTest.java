9894bf0872fff2f0388f365bd5907c07ccc8c37e
Herman Bimikwe <bimikwe.herman@gmail.com>
2022-07-01 11:19:03 -0500

    Adding some unit testing for user input AB#729

---
+++ test/com/games/gobigorgohome/app/UserInputTest.java
@@ -0,0 +1,13 @@
+package com.games.gobigorgohome.app;
+
+import junit.framework.TestCase;
+
+public class UserInputTest extends TestCase {
+
+
+    public void testPromptShouldOutputGameQuestionInJLabel() {
+    }
+
+    public void testDisplayShouldShowGameMessageInJTextArea() {
+    }
+}
