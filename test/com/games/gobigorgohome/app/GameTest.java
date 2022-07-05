bd24f6bab42c1636e028804ab7d3771cf55c2db8
mlmirk <mike.l.mirkovic@gmail.com>
2022-07-05 13:18:07 -0700

    added player test case and boundry for lower than zero energy fix typo, AB#740

--- test/com/games/gobigorgohome/app/GameTest.java
+++ test/com/games/gobigorgohome/app/GameTest.java
@@ -2,14 +2,12 @@ package com.games.gobigorgohome.app;
 
 import com.apps.util.Prompter;
 import org.json.simple.parser.ParseException;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 
 import static org.junit.Assert.*;
@@ -17,13 +15,10 @@ import static org.junit.Assert.*;
 public class GameTest {
     List requiredItems = new ArrayList();
     List noRequiredItems = new ArrayList();
-    Scanner in = new Scanner(System.in);
-    Prompter prompter=new Prompter(in);
-    Game game= new Game(prompter);
 
     public GameTest() throws IOException, ParseException {
-    }
 
+    }
 
     @Before
     public void createRequiredItemList() {
@@ -43,12 +38,6 @@ public class GameTest {
     @Test
     public void isItemRequiredShouldReturnTrueIfItemsAreRequired() {
         assertTrue(Game.isItemRequired(requiredItems));
-    }
-    @Test
-    public void TravelingToNotValidLocationThrowsProperString() throws IOException, ParseException {
-        String[] test = {"go","west"};
-        game.parseThroughPlayerInput(test);
-        //Assert.assertEquals(game.getCurrentRoom(), null);
     }
 
 
